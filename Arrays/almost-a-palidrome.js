51ba99194615324fcfbee6634f7a0f03d566b4c0
Brutalpsy <brutalpsy89@gmail.com>
2021-11-28 16:17:41 +0100

    commented out optimal solution #2

--- Arrays/almost-a-palidrome.js
+++ Arrays/almost-a-palidrome.js
@@ -33,43 +33,43 @@ var validPalindrome = function (s) {
 
 // optimal solution #2
 // O(n) time, O(1) space
-var validPalindrome = function (s) {
-  let leftUsed = false;
-  let rightUsed = false;
-  let orginalString = s;
-  let newString = '';
-  let p1 = 0;
-  let p2 = s.length - 1;
-  let p2Old = 0;
-  let p1Old = 0;
+// var validPalindrome = function (s) {
+//   let leftUsed = false;
+//   let rightUsed = false;
+//   let orginalString = s;
+//   let newString = '';
+//   let p1 = 0;
+//   let p2 = s.length - 1;
+//   let p2Old = 0;
+//   let p1Old = 0;
 
-  while (p1 <= p2) {
-    if (s[p1] != s[p2]) {
-      if (!leftUsed) {
-        leftUsed = true;
-        p2Old = p2;
-        p1Old = p1;
-        newString = orginalString.slice(0, p1) + orginalString.slice(p1 + 1);
-        p2 = p2Old - 1;
-        s = newString;
-      } else if (!rightUsed) {
-        rightUsed = true;
-        p1 = p1Old;
-        newString =
-          orginalString.slice(0, p2Old) + orginalString.slice(p2Old + 1);
-        s = newString;
-        p2 = p2Old - 1;
-      } else {
-        return false;
-      }
-    } else {
-      p1++;
-      p2--;
-    }
-  }
+//   while (p1 <= p2) {
+//     if (s[p1] != s[p2]) {
+//       if (!leftUsed) {
+//         leftUsed = true;
+//         p2Old = p2;
+//         p1Old = p1;
+//         newString = orginalString.slice(0, p1) + orginalString.slice(p1 + 1);
+//         p2 = p2Old - 1;
+//         s = newString;
+//       } else if (!rightUsed) {
+//         rightUsed = true;
+//         p1 = p1Old;
+//         newString =
+//           orginalString.slice(0, p2Old) + orginalString.slice(p2Old + 1);
+//         s = newString;
+//         p2 = p2Old - 1;
+//       } else {
+//         return false;
+//       }
+//     } else {
+//       p1++;
+//       p2--;
+//     }
+//   }
 
-  return true;
-};
+//   return true;
+// };
 
 ['raceacar', 'abccdba', 'abcdefdba', '', 'a', 'ab'].forEach((string) =>
   console.log(string, validPalindrome(string))
